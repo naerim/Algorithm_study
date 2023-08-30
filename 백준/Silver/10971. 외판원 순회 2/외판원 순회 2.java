aba9b2f8e60884999862e6b2ff69543930334f80
김내림 <naerim1119@gmail.com>
2023-08-30 17:42:35 +0900

    [Silver II] Title: 외판원 순회 2, Time: 336 ms, Memory: 13200 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -1,6 +1,16 @@
+/**
+ * BJ 10971 외판원순회2
+ * 
+ * 순열을 통해 순서를 정한 후 서로 인접해 있는지 확인한다.
+ * 
+ * 메모리 12948 KB
+ * 시간 364 ms
+ * 
+ * @author 김내림
+ * 
+ */
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -22,11 +32,11 @@ public class Main {
 			}
 		}
 		
-		input = new int[N];
-		select = new boolean[N];
+		input = new int[N]; // 뽑은 숫자를 저장할 배열
+		select = new boolean[N]; // 방문 여부 저장할 배열
 		
-		min = Integer.MAX_VALUE;
-		permutation(0);
+		min = Integer.MAX_VALUE; // 최솟값을 찾아야하기 때문에 가장 큰 값으로 초기화
+		permutation(0); // 순열 실행
 		System.out.println(min);
 	}
 	
@@ -35,13 +45,14 @@ public class Main {
 		if(cnt==N) {
 			int total = 0;
 			for(int i=1; i<N; i++) {
-				if(arr[input[i-1]][input[i]] == 0) return;
+				if(arr[input[i-1]][input[i]] == 0) return; // 연결되어 있지 않으면 계산 안하고 바로 종료
 				total += arr[input[i-1]][input[i]];
+				if(total>min) return;
 			}
-			if(arr[input[N-1]][input[0]] == 0) return;
+			if(arr[input[N-1]][input[0]] == 0) return; // 연결되어 있지 않으면 계산 안하고 바로 종료
 			
 			total += arr[input[N-1]][input[0]];
-			min = Math.min(min, total);
+			min = Math.min(min, total); // 최소값 갱신
 			return;
 		}
 		
