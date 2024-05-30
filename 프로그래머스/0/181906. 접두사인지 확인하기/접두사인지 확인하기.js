6b6254d1b65fd3782120e84bb1d08fb95a4d99df
김내림 <naerim1119@gmail.com>
2024-05-31 02:06:17 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,10 +1,3 @@
 function solution(my_string, is_prefix) {
-    var answer = 0;
-    
-    for(let i=0; i<is_prefix.length; i++) {
-        if(my_string[i] !== is_prefix[i]){
-            return 0;
-        }
-    }
-    return 1;
+    return my_string.startsWith(is_prefix) ? 1 : 0;
 }
