5ad213cabcf5e31976f48d3e3332d786d960110a
김내림 <naerim1119@gmail.com>
2024-06-01 20:43:54 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -0,0 +1,17 @@
+function solution(n) {
+    let answer = 0;
+    
+    if(n%2===1) {
+        for(let i=n; i>0; i-=2) {
+            answer +=i
+        }
+    } else {
+        for(let i=n; i>0; i-=2) {
+            answer +=i**2
+        }
+    }
+    
+
+    
+    return answer;
+}
