9d66b13dda086d334fb4254c4bee76b195cfd3d8
김내림 <naerim1119@gmail.com>
2024-06-01 21:22:38 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,9 @@
+function solution(a, b) {
+    let answer = 0;
+    if(a%2===1 && b%2===1) {
+        answer = a**2 + b**2
+    } else if(a%2===0 && b%2===0) {
+        answer = Math.abs(a - b)
+    } else answer = 2*(a+b)
+    return answer;
+}
