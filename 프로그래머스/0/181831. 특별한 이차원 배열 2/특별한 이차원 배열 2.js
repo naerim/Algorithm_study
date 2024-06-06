618f7d6dfdf6dbe28ed2d2529c6d0d6afdedebc6
김내림 <naerim1119@gmail.com>
2024-06-06 23:40:25 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -0,0 +1,9 @@
+function solution(arr) {
+    let len = arr[0].length
+    for(let i=0; i<len; i++) {
+        for(let j=0; j<len; j++) {
+            if (arr[i][j] !== arr[j][i]) return 0
+        }
+    }
+    return 1
+}
