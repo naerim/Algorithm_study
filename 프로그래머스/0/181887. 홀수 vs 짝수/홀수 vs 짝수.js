ec24228df15713cedaba5f71828ee65e6b9592c2
김내림 <naerim1119@gmail.com>
2024-06-07 23:38:42 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,9 @@
+function solution(num_list) {
+
+    let odd = 0;
+    let even = 0;
+    num_list.forEach((v,i) => {
+        i % 2 === 0 ? even += v : odd += v
+    })
+    return odd > even ? odd : even;
+}
