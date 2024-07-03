6c0bd026a366a6fa08c6c74bb2fdc22e488f476f
김내림 <naerim1119@gmail.com>
2024-07-03 23:50:31 +0900

    [Silver III] Title: 퇴사, Time: 128 ms, Memory: 9600 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.js
+++ 백준/Silver/14501. 퇴사/퇴사.js
@@ -5,16 +5,15 @@ const [n, ...arr] = fs.readFileSync("/dev/stdin").toString().split("\n");
 const N = Number(n);
 const list = arr.map((v) => v.split(" ").map((v) => +v));
 
-let dp = Array(N).fill(0);
+let dp = Array(N + 1).fill(0);
 
+// i번째 일했을 때 얻는 최대 수익
 for (let i = 0; i < N; i++) {
   const [period, value] = list[i];
-  if (i + period > N) continue;
-  dp[i] += value;
-
-  for (let j = i + period; j < N; j++) {
-    dp[j] = Math.max(dp[i], dp[j]);
+  // j는 상담이 가능한 모든 날짜
+  for (let j = i + period; j < N + 1; j++) {
+    if (dp[j] < dp[i] + value) dp[j] = dp[i] + value;
   }
 }
 
-console.log(Math.max(...dp));
+console.log(dp[N]);
