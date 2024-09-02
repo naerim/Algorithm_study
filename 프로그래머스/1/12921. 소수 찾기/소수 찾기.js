2af1504c71e44daae7ad29696757049749e78dcf
김내림 <naerim1119@gmail.com>
2024-09-02 23:29:51 +0900

    [level 1] Title: 소수 찾기, Time: 576.12 ms, Memory: 87 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,18 @@
+function solution(n) {
+  const s = new Set();
+  for (let i = 2; i <= n; i++) {
+    s.add(i);
+  }
+
+  // 2부터 n의 제곱근보다 작은 최대 정수까지 
+  for (let j = 2; j < Math.sqrt(n); j++) {
+    // Set에 해당 수가 포함되면
+    if (s.has(j)) {
+      // 그 수를 제외하고 그 수의 배수는 모두 삭제
+      for (let k = j * 2; k <= n; k += j) {
+        s.delete(k);
+      }
+    }
+  }
+  return s.size;
+}
